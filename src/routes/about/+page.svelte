7bd832d3436a749da58564f1a41915707d4b7d72
Noah Weber <noah@noahw.design>
2024-07-24 02:08:56 -0400

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col w-full h-full items-center justify-center">
   <p class="text-left font-montserrat w-96">
-    Open Marching is an organization, dedicated to creating free and open
-    software for the marching arts, allowing groups to allocate more money
-    towards their students/ participants, rather than their software.
+    Open Marching is an organization, dedicated to creating and curating, free
+    and open software for the marching arts, allowing groups to allocate more
+    resources towards their students and participants, rather than software.
   </p>
 </div>
